f546b2b219d28fe74737db951b2c96d1d1433a43
umayer16 <muktadirumayer30@gmail.com>
2025-11-25 23:12:31 +0600

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+// src/app/page.tsx (full example)
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to NoteNest</h1>
+      <p>Your dashboard loads here!</p>
+    </div>
+  );
+}
